ee6917d445da94ca3352c1ba003d940a4f457539
hitz patel <destiny.creator1959@gmail.com>
2023-10-25 22:50:31 +0100

    week 7

---
+++ week7/src/javahw7/Ex15.java
@@ -0,0 +1,14 @@
+package javahw7;
+
+public class Ex15 {
+
+    public static void main(String[] args)
+    {
+        String name = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+
+        System.out.println(name);
+
+        name = name.toLowerCase();
+        System.out.println(name);
+    }
+}
