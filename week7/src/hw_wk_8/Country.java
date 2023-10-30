46951858d72128f96a239cb3c8415987be379782
hites <destiny.creator1959@gmail.com>
2023-10-30 22:46:18 +0000

    wk8

--- week7/src/hw_wk_8/Country.java
+++ week7/src/hw_wk_8/Country.java
@@ -1,2 +1,27 @@
-package hw_wk_8;public class Country {
+package hw_wk_8;
+
+public class Country {
+    public static void main(String[] args) {
+        String []country={"India","Nepal","England","France","Spain",};
+
+        String []capital={"NEW DELHI","Kathmandu","London","PARIS","MADRID"};
+
+        int count=0;
+
+        while(count<5)
+
+        {
+            System.out.print(country[count] +"  ");
+
+            System.out.print(capital[count]);
+
+            System.out.println();
+
+            count++;
+
+        }
+
+
+
+    }
 }
