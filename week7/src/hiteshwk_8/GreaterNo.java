453d0d152bb5d2bf04fd12d3439c0e2ab4326e40
hites <destiny.creator1959@gmail.com>
2023-10-30 20:22:14 +0000

    week 8

---
+++ week7/src/hiteshwk_8/GreaterNo.java
@@ -0,0 +1,16 @@
+package hiteshwk_8;
+//Take two int values from user and print greatest among them.
+
+public class GreaterNo {
+    public static void main(String[] args) {
+        int num1 = 27, num2 = 9;
+        if (num1 == num2)
+            System.out.println ("both are equal");
+        else if (num1 > num2)
+            System.out.println (num1 + " is greater");
+
+        else
+            System.out.println (num2 + " is greater");
+
+    }
+}
