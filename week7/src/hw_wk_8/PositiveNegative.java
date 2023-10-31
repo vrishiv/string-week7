378eee85c0349c967a9213a14db868150d7abaf1
hites <destiny.creator1959@gmail.com>
2023-10-31 20:00:06 +0000

    wk8

---
+++ week7/src/hw_wk_8/PositiveNegative.java
@@ -0,0 +1,31 @@
+package hw_wk_8;
+
+import java.util.Scanner;
+
+public class PositiveNegative {
+    public static void main(String[] args) {
+        int x;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the number you want to check:");
+
+        x = s.nextInt();
+
+        if(x > 0)
+
+        {
+            System.out.println("The given number "+x+" is Positive");
+        }
+
+        else if(x < 0)
+        {
+            System.out.println("The given number "+x+" is Negative");
+        }
+
+        else
+        {
+            System.out.println("The given number "+x+" is neither Positive nor Negative ");
+        }
+    }
+}
