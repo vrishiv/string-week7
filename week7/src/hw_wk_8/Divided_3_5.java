13d0dd857ccfbd53e402e928fe97a286806a5139
hites <destiny.creator1959@gmail.com>
2023-11-01 21:32:27 +0000

    wk8

---
+++ week7/src/hw_wk_8/Divided_3_5.java
@@ -0,0 +1,29 @@
+package hw_wk_8;
+
+public class Divided_3_5 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("\nDivided by 3: ");
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 == 0)
+                System.out.print(i + ", ");
+        }
+
+
+        System.out.println("\n\nDivided by 5: ");
+        for (int i = 1; i < 100; i++) {
+            if (i % 5 == 0)
+                System.out.print(i + ", ");
+        }
+
+
+        System.out.println("\n\nDivided by 3 & 5: ");
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.print(i + ", ");
+        }
+        System.out.println("\n");
+    }
+}
